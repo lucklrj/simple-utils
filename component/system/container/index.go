d7adf94ca5bb6acf892f4d603e7cb8676ced0fce
lucklrj <sunny_lrj@yeah.net>
2023-03-21 09:30:17 +0800

    支持过期时间

--- component/system/container/index.go
+++ component/system/container/index.go
@@ -1,26 +1,53 @@
 package container
 
 import (
+	dateHelper "github.com/lucklrj/simple-utils/helper/date"
 	"github.com/orcaman/concurrent-map/v2"
 )
 
 type Container struct {
 	data cmap.ConcurrentMap[interface{}]
 }
+type singleData struct {
+	ExpireTime uint64
+	Data       interface{}
+}
 
 func (c *Container) Init() {
 	c.data = cmap.New[interface{}]()
 }
-func (c *Container) Set(key string, value interface{}) {
-	c.data.Set(key, value)
-
+func (c *Container) Set(key string, value interface{}, expire uint64) {
+	var expireTime uint64 = 0
+	if expire > 0 {
+		expireTime = dateHelper.GetNowUnixTimeStamp() + expire
+	}
+	data := singleData{
+		ExpireTime: expireTime,
+		Data:       value,
+	}
+	c.data.Set(key, data)
 }
 func (c *Container) Check(key string) bool {
-	return c.data.Has(key)
+	if c.data.Has(key) == false {
+		return false
+	} else {
+		obj, _ := c.data.Get(key)
+		value := obj.(singleData)
+		if value.ExpireTime > 0 && value.ExpireTime < dateHelper.GetNowUnixTimeStamp() {
+			return false
+		} else {
+			return true
+		}
+	}
 }
 func (c *Container) Get(key string, defaultValue interface{}) interface{} {
-	value, _ := c.data.Get(key)
-	return value
+	obj, _ := c.data.Get(key)
+	value := obj.(singleData)
+	if value.ExpireTime > 0 && value.ExpireTime < dateHelper.GetNowUnixTimeStamp() {
+		return defaultValue
+	} else {
+		return obj.(singleData).Data
+	}
 
 }
 
