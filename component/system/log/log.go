96ad1395493bd8f6ac8395aa5a9752e4e4c4a323
lucklrj <sunny_lrj@yeah.net>
2023-03-21 09:48:16 +0800

    封装日志目录

--- component/system/log/log.go
+++ component/system/log/log.go
@@ -1,6 +1,9 @@
 package log
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/lestrrat-go/file-rotatelogs"
 	log "github.com/sirupsen/logrus"
 )
@@ -8,9 +11,15 @@ import (
 var Logger *log.Logger
 
 func Run(logPath string) error {
+	binPath, err := os.Executable()
+	if err != nil {
+		return err
+	}
+
+	logPath = filepath.Dir(binPath) + "/" + logPath
 
 	writer, err := rotatelogs.New(
-		logPath+".%Y-%m-%d",
+		logPath+"-%Y-%m-%d",
 		rotatelogs.WithRotationTime(1),
 	)
 	if err != nil {
