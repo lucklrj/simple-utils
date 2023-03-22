fea6e0a830fbb2478e33901217f712c755978507
lucklrj <sunny_lrj@yeah.net>
2023-03-22 15:35:17 +0800

    env修改

--- component/system/env/env.go
+++ component/system/env/env.go
@@ -2,9 +2,7 @@ package env
 
 import (
 	"flag"
-	"os"
 
-	"github.com/fatih/color"
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
@@ -16,25 +14,24 @@ func init() {
 	envPath = flag.String("env_path", "./.env", "配置文件地址")
 	flag.Parse()
 }
-func ParseEnv() {
-
-	color.Green("开始解析.env配置文件")
+func Load(callback func(map[string]string)) error {
 
 	viper.SetConfigFile(*envPath)
 	err := viper.ReadInConfig()
 	if err != nil {
-		color.Red("解析.env文件错误：" + err.Error())
-		os.Exit(0)
+		return err
 	}
-	load()
+
+	load(callback)
+
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
-		load()
+		load(callback)
 	})
-
+	return nil
 }
-func load() {
+func load(callback func(map[string]string)) {
 	var result map[string]string
 	viper.Unmarshal(&result)
-	Envs = result
+	callback(result)
 }
