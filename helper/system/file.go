cb9923e425532b7147a94d02454b79ef8dc8c914
lucklrj <sunny_lrj@yeah.net>
2023-05-23 10:14:36 +0800

    增加写入文件操作

--- helper/system/file.go
+++ helper/system/file.go
@@ -3,6 +3,7 @@ package system
 import (
 	"bufio"
 	"io"
+	"os"
 )
 
 func ReadAllFromFile(file io.Reader) ([]string, error) {
@@ -23,4 +24,17 @@ func ReadAllFromFile(file io.Reader) ([]string, error) {
 		}
 	}
 	return result, nil
+}
+
+func WriteFile(filename string, data []byte, perm os.FileMode) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	n, err := f.Write(data)
+	if err == nil && n < len(data) {
+		return io.ErrShortWrite
+	}
+	return nil
 }
