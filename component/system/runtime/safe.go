f525523fb6e6949bff7fc2de7a52f0f089e97353
lucklrj <sunny_lrj@yeah.net>
2023-06-15 16:44:42 +0800

    增加日志输出

--- component/system/runtime/safe.go
+++ component/system/runtime/safe.go
@@ -1,11 +1,26 @@
 package runtime
 
-import "github.com/zeromicro/go-zero/core/rescue"
+import (
+	"runtime/debug"
+
+	"github.com/lucklrj/simple-utils/component/system/log"
+)
 
 func GoSafe(fn func()) {
 	go RunSafe(fn)
 }
 func RunSafe(fn func()) {
-	defer rescue.Recover()
+	defer Recover()
 	fn()
+}
+
+func Recover(cleanups ...func()) {
+	for _, cleanup := range cleanups {
+		cleanup()
+	}
+
+	if p := recover(); p != nil {
+		log.Logger.Error(p)
+		log.Logger.Error(string(debug.Stack()))
+	}
 }
