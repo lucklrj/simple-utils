3e932bfad80c92ca7f433f756876d047e3b959e4
lucklrj <sunny_lrj@yeah.net>
2023-06-24 18:55:56 +0800

    添加队列

---
+++ component/system/queue/secure_queue.go
@@ -0,0 +1,69 @@
+package quenue
+
+import (
+	"container/list"
+	"sync"
+)
+
+type SecureQueue struct {
+	data *list.List
+	size int
+	lock sync.Mutex
+}
+
+func MakeDefault() *SecureQueue {
+	q := new(SecureQueue)
+	q.init()
+	return q
+}
+
+func (q *SecureQueue) init() {
+	q.data = list.New()
+}
+
+func (q *SecureQueue) Size() int {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	return q.size
+}
+
+func (q *SecureQueue) IsEmpty() bool {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	return q.size == 0
+}
+
+func (q *SecureQueue) Front() interface{} {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	return q.data.Front().Value
+}
+
+func (q *SecureQueue) Back() interface{} {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	return q.data.Back().Value
+}
+
+func (q *SecureQueue) Push(value ...interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	for _, single := range value {
+		q.data.PushBack(single)
+	}
+	q.size++
+}
+
+func (q *SecureQueue) Pop() interface{} {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	if q.size > 0 {
+		tmp := q.data.Back()
+		q.data.Remove(tmp)
+		q.size--
+		return tmp
+	} else {
+		return nil
+	}
+
+}
