2f66e2c78b3b34246c23979e770ee1b2140bf97d
lucklrj <sunny_lrj@yeah.net>
2023-08-30 15:50:36 +0800

    增加struct复制的兼容性

--- helper/system/struct.go
+++ helper/system/struct.go
@@ -3,6 +3,8 @@ package system
 import (
 	"errors"
 	"reflect"
+
+	"github.com/spf13/cast"
 )
 
 func StructCopyTo(src, dst interface{}) error {
@@ -18,9 +20,49 @@ func StructCopyTo(src, dst interface{}) error {
 	for i := 0; i < srcValue.NumField(); i++ {
 		srcField := srcValue.Field(i)
 		dstField := dstElem.FieldByName(srcValue.Type().Field(i).Name)
-		if dstField.IsValid() && dstField.Type() == srcField.Type() {
+		if dstField.Type() == srcField.Type() {
 			dstField.Set(srcField)
+		} else { //手动抓换
+			value, err := changeType(dstField.Type().String(), srcField.Interface())
+			if err == nil {
+				dstField.Set(reflect.ValueOf(value))
+			}
 		}
 	}
 	return nil
+}
+
+func changeType(targetType string, value interface{}) (interface{}, error) {
+	switch targetType {
+	case "string":
+		return cast.ToString(value), nil
+	case "bool":
+		return cast.ToBool(value), nil
+	case "int":
+		return cast.ToInt(value), nil
+	case "int8":
+		return cast.ToInt8(value), nil
+	case "int16":
+		return cast.ToInt16(value), nil
+	case "int32":
+		return cast.ToInt32(value), nil
+	case "int64":
+		return cast.ToInt64(value), nil
+	case "uint":
+		return cast.ToUint(value), nil
+	case "uint8":
+		return cast.ToUint8(value), nil
+	case "uint16":
+		return cast.ToUint16(value), nil
+	case "uint32":
+		return cast.ToUint32(value), nil
+	case "uint64":
+		return cast.ToUint64(value), nil
+	case "float32":
+		return cast.ToFloat32(value), nil
+	case "float64":
+		return cast.ToFloat64(value), nil
+	default:
+		return nil, errors.New("不支持该类型的转换")
+	}
 }
