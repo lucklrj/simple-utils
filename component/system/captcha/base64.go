b2cda163b919d52f04abee2754d03c745f169249
lucklrj <sunny_lrj@yeah.net>
2023-09-11 09:47:14 +0800

    修改存储方式

--- component/system/captcha/base64.go
+++ component/system/captcha/base64.go
@@ -1,34 +1,41 @@
 package captcha
 
 import (
+	"sync"
 	"time"
 
 	"github.com/mojocn/base64Captcha"
+	"github.com/spf13/cast"
 )
 
 type Store struct {
-	data map[string]string
+	data sync.Map
 }
 
 func (s *Store) Set(id string, value string) error {
 	defer func() {
 		time.AfterFunc(5*time.Minute, func() {
-			delete(s.data, id)
+			s.data.Delete(id)
 		})
 	}()
-	s.data[id] = value
+	s.data.Store(id, value)
 	return nil
 }
 func (s *Store) Get(id string, clear bool) string {
 	defer func() {
 		if clear {
-			delete(s.data, id)
+			s.data.Delete(id)
 		}
 	}()
-	return s.data[id]
+	value, isExists := s.data.Load(id)
+	if isExists {
+		return cast.ToString(value)
+	} else {
+		return ""
+	}
 }
 func (s *Store) Verify(id, answer string, clear bool) bool {
-	return answer == s.data[id]
+	return answer == s.Get(id, clear)
 }
 
 func MakeBase64CaptchaClient() *base64Captcha.Captcha {
@@ -40,7 +47,6 @@ func MakeBase64CaptchaClient() *base64Captcha.Captcha {
 	driver.DotCount = 100
 
 	store := Store{}
-	store.data = make(map[string]string)
 
 	return base64Captcha.NewCaptcha(&driver, &store)
 
